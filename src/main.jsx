2fa8739cb69e9e17a01a4b73bed1b762cc52064c
Mutalib627 <139040376+Mutalib627@users.noreply.github.com>
2026-08-22 16:17:42 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CoopGuardSim from "./CoopGuardSim.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CoopGuardSim />
-  </React.StrictMode>
-);
